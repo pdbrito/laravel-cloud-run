a56e3aabf9e9805d1a5f1587fbab0763f1538854
Pedro de Brito <pedro.debrito@wonderkind.com>
2019-08-28 11:53:19 +0300

    Container only has one role

--- devops/docker/entrypoint.sh
+++ devops/docker/entrypoint.sh
@@ -2,28 +2,5 @@
 
 set -e
 
-role=${CONTAINER_ROLE:-app}
-env=${APP_ENV:-production}
-
-if [[ "$role" = "app" ]]; then
-
-    php /var/www/html/artisan migrate --force
-    docker-php-entrypoint apache2-foreground
-
-elif [[ "$role" = "worker" ]]; then
-
-    echo "Running the worker..."
-    php /var/www/html/artisan horizon
-
-elif [[ "$role" = "scheduler" ]]; then
-
-    while [[ true ]]
-    do
-      php /var/www/html/artisan schedule:run --verbose --no-interaction &
-      sleep 60
-    done
-
-else
-    echo "Could not match the container role \"$role\""
-    exit 1
-fi
+php /var/www/html/artisan migrate --force
+docker-php-entrypoint apache2-foreground
