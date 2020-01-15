8f8d76f9c42003aa1dbac7b37d1e3b7e4bc0656e
Pedro de Brito <ped.de.brito@gmail.com>
2020-01-15 19:44:07 +0100

    Clean up entrypoint.sh

--- devops/docker/entrypoint.sh
+++ devops/docker/entrypoint.sh
@@ -2,29 +2,8 @@
 
 set -e
 
-role=${CONTAINER_ROLE:-app}
 env=${APP_ENV:-production}
 port=${PORT:-8080}
 
-if [[ "$role" = "app" ]]; then
-
-    sed -i "s/\$PORT/$port/g" /etc/nginx/nginx.conf
-    exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
-
-elif [[ "$role" = "worker" ]]; then
-
-    echo "Running the worker..."
-    php /var/www/html/artisan horizon
-
-elif [[ "$role" = "scheduler" ]]; then
-
-    while [[ true ]]
-    do
-      php /var/www/html/artisan schedule:run --verbose --no-interaction &
-      sleep 60
-    done
-
-else
-    echo "Could not match the container role \"$role\""
-    exit 1
-fi
+sed -i "s/\$PORT/$port/g" /etc/nginx/nginx.conf
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
