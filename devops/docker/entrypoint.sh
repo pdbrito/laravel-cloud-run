4c6addef0b399f27f24dece1ea4d9dbb90fa7b8f
Pedro de Brito <ped.de.brito@gmail.com>
2020-01-16 19:05:06 +0100

    Get rid of s6

--- devops/docker/entrypoint.sh
+++ devops/docker/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-set -e
+set -euxo pipefail
 
 env=${APP_ENV:-production}
 port=${PORT:-8080}
 
 sed -i "s/\$PORT/$port/g" /etc/nginx/nginx.conf
-exec /init
+
+PID_FILE=/tmp/php-fpm.pid
+php-fpm -g $PID_FILE -D
+while [ ! -f $PID_FILE ]; do sleep 0.1; done
+nginx -g 'daemon off;'
