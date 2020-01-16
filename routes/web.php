b6de3a9e2b08521012e492403679fc010e404c66
Pedro de Brito <ped.de.brito@gmail.com>
2020-01-16 19:34:04 +0100

    Add some routes for testing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Log;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +16,18 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/inspire', function () {
+    return Inspiring::quote();
+});
+
+Route::get('/log', function () {
+    Log::info('Info');
+    return response('Ok');
+});
+
+Route::get('/error', function () {
+    Log::warning('Hold up');
+    Throw new Exception();
 });
